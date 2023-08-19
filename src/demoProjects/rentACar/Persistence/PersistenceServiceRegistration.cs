55ba26d45f47ddaa89f8eca76565e16f78818563
ebaykalgurbuz <ebaykalgurbuz@gmail.com>
2023-08-19 14:27:07 +0300

    Created brand repository and connected db

--- src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
+++ src/demoProjects/rentACar/Persistence/PersistenceServiceRegistration.cs
@@ -1,14 +1,10 @@
-﻿using Application.Services.Repositories;
+﻿
+using Application.Services.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Persistence.Contexts;
 using Persistence.Repositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Persistence
 {
@@ -21,11 +17,6 @@ namespace Persistence
                                                      options.UseSqlServer(
                                                          configuration.GetConnectionString("RentACarCampConnectionString")));
             services.AddScoped<IBrandRepository, BrandRepository>();
-            services.AddScoped<IModelRepository, ModelRepository>();
-            services.AddScoped<IUserRepository,UserRepository>();
-            services.AddScoped<IRefreshTokenRepository, RefreshTokenRepository>();
-            services.AddScoped<IOperationClaimRepository, OperationClaimRepository>();
-            services.AddScoped<IUserOperationClaimRepository, UserOperationClaimRepository>();
 
             return services;
         }
